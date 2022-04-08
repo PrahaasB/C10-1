7e8697cc30e37ecae30a15e721db12c255f1f629
PrahaasB <102772947+PrahaasB@users.noreply.github.com>
2022-04-07 19:20:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var weights=[38, 42, 49, 55, 28]
+sum=0
+avg=0 
+function setup() 
+{
+  createCanvas(400,400);
+sum=weights[0]+weights[1]+weights[2]+weights[3]+weights[4]
+avg=sum/weights.length
+console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
